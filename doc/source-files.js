9f129f06732da13dfd25e79afb4720487bee2cec
Deployment Bot (from Azure Pipelines) <deploy@tokio-rs.com>
2019-09-08 20:16:29 +0000

    Deploy Bytes API documentation

---
+++ doc/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["bytes"] = {"name":"","dirs":[{"name":"buf","files":["buf_impl.rs","buf_mut.rs","chain.rs","iter.rs","mod.rs","reader.rs","take.rs","vec_deque.rs","writer.rs"]}],"files":["bytes.rs","debug.rs","hex.rs","lib.rs"]};
+createSourceSidebar();
